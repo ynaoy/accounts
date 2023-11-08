cd3a7baf0e2bc6a632798bca4154959c25f9fb73
ynaoy <naozou22113@yahoo.co.jp>
2023-11-09 03:17:06 +0900

    djangoのmain_appにutils.pyとそのテストを追加。

--- django/src/django_accounts/main_app/tests.py
+++ django/src/django_accounts/main_app/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .test.models_tests import UserModelTests
+from .test.utils_tests import UserUtilsTests
 
 class Tests(TestCase):
   UserModelTests()
+  UserUtilsTests()
