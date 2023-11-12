a7ab880f6832da5de73b3f67be2d2ea616d04ace
ynaoy <naozou22113@yahoo.co.jp>
2023-11-12 10:00:06 +0900

    nextjsのcomponentsにbuttons,formsを追加。そのテストを追加

---
+++ nextjs/src/nextjs_accounts/components/forms/types/Form.d.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export type FormProps = { 
+  onClick?: (event:React.MouseEvent<HTMLButtonElement>)=>void,
+  buttonText:string, 
+  children?: React.ReactNode}
+
+export type FormItemProps = { 
+  onChange?: (event:React.ChangeEvent<HTMLInputElement>)=>void,
+  id: string,
+  labelText:string, 
+  type?:string }
---
+++ nextjs/src/nextjs_accounts/tests/components/FormItem.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import FormItem from "../../components/forms/FormItem"
+
+const onChangeMock = jest.fn()
+describe("FormItem", ()=>{
+  test("フォームが存在するかテスト", () => {
+    //レンダー
+    render(<FormItem onChange={onChangeMock} id="test" type="text" labelText="テスト" ></FormItem>);
+
+    //labelTextが表示されている
+    expect(screen.getByLabelText("テスト")).toBeTruthy()
+    
+    //フォームのtype属性が適応されている
+    expect(screen.getByRole("textbox")).toBeTruthy()
+  });
+
+  test('フォームのに入力があった時にonChangeが呼び出される',async()=>{
+    //レンダー
+    render(<FormItem onChange={onChangeMock} id="test" type="text" labelText="テスト" ></FormItem>);
+    
+    //フォームに値を入力
+    const form = screen.getByRole('textbox')
+    await userEvent.type(form, "テストインパット");
+    
+    //フォームに値が入力されている
+    expect(form.value).toBe("テストインパット")
+    
+    //onChangeイベントが発火している
+    expect(onChangeMock).toHaveBeenCalled()
+  })
+})
