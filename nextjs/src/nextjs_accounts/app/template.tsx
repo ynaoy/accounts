bf7fc8888c272383242df83f08bea31bf66e758f
ynaoy <naozou22113@yahoo.co.jp>
2023-11-14 04:53:01 +0900

    nextjsでlib/apiHelper.tsxを追加

--- nextjs/src/nextjs_accounts/app/template.tsx
+++ nextjs/src/nextjs_accounts/app/template.tsx
@@ -1,17 +1,32 @@
+"use client";
 /**
  * コンポーネントをラップするテンプレート
  * 画面の遷移時に再レンダーされる
  */
 import 'tailwindcss/tailwind.css'
+import { useEffect } from 'react'
+import { fetchLoginFlg } from '../lib/apiHelper'
+import { LoginFlgContextProvider, useSetLoginFlgContext } from '../hooks/LoginFlgContext'
 
-export default function RootTemplate({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootTemplate({children,}: {children: React.ReactNode}) {
+  
+  // グローバル変数からloginFlgの更新用関数を取得
+  const setLoginFlg = useSetLoginFlgContext()
+
+  // APIと通信して、LoginFlgを更新する関数
+  const fetchData = async()=>{
+    let {loginFlg} = await fetchLoginFlg()
+    setLoginFlg(loginFlg)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div className="bg-white dark:bg-slate-800 flex flex-col justify-center items-center h-screen w-screen">
-      {children}
+      <LoginFlgContextProvider>
+        {children}
+      </LoginFlgContextProvider>
     </div>
   )
 }
---
+++ nextjs/src/nextjs_accounts/lib/apiHelper.tsx
@@ -0,0 +1,38 @@
+import {fetchLoginFlgReturn,fetchParams} from './types/apiHelper.d'
+
+export const fetchResponseFromApi = async(
+  url:string, 
+  params:fetchParams, 
+  data: Record<string, unknown> = {}
+) =>{
+  // Json形式でリクエストをする
+  params.headers['Content-Type'] = 'application/json';
+
+  // GETメソッド時以外の時はパラメータをbodyに貼り付ける
+  if(params.method!='GET'){
+    params.body = JSON.stringify(data);
+  }
+  // APIにリクエストを送ってレスポンスをもらう
+  let response = await fetch(url, params)
+  return response
+}
+
+export const fetchLoginFlg:fetchLoginFlgReturn =async()=>{
+  /**
+   * @return {boolean} - ログイン状態を返す
+   */
+
+  // APIにリクエストを送る
+  try{
+    const response = await fetchResponseFromApi(`${process.env.NEXT_PUBLIC_API_ORIGIN}/api/is_login`,
+    { method:'GET',
+      headers: {},
+      credentials: 'include',
+    })
+    return response.json()
+  
+  } catch(error) {
+    console.error(error);
+    return {loginFlg: false}
+  }
+}
