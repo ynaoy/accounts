d03102c795411f9ff551f5c9337e4437ece6069f
ynaoy <naozou22113@yahoo.co.jp>
2024-04-18 02:50:37 +0900

    今後実装予定のLoginコンポーネント内で使用するロジックをまとめたuseLoginフックを追加した。そのテストを追加した

---
+++ nextjs/src/nextjs_accounts/hooks/useLogin.tsx
@@ -0,0 +1,93 @@
+import { useReducer } from 'react';
+import { useLoginFlgContext, useSetLoginFlgContext } from './LoginFlgContext';
+import { initialState as formInitialState, useFormReducer } from './useFormReducer';
+import { initialState as validationInitialState, useFormValidationReducer,
+         checkEmail, checkPassword } from './useFormValidationReducer';
+import { useCustomRouter } from './useCustomRouter';
+import { useFetch } from '../hooks/useFetch';
+
+export const useLogin = () => {
+  // ページ遷移を管理するルーター
+  const { redirectToIndexPage } = useCustomRouter()
+  // APIと通信する関数を受け取る
+  const { postToLoginApi } = useFetch()
+  // ログイン状態と更新関数をコンテキストから取得する 
+  const loginFlg = useLoginFlgContext();
+  const setLoginFlg = useSetLoginFlgContext()
+  // フォームの状態を管理するリデューサ
+  const [formState, formDispatch] = useReducer(useFormReducer, formInitialState)
+  // バリデーションの状態を管理するリデューサ
+  const [validationStates, validationDispatch] = useReducer(useFormValidationReducer, validationInitialState)
+
+  const validateForm = () => {
+    /**
+     * 全てのバリデーションをチェック
+     * @return {boolean}
+     */
+
+    // 各項目のバリデーションをチェックし、結果をオブジェクトに格納
+    const validations = {
+      ...validationStates,
+      emailValidations: checkEmail(formState['email']),
+      passwordValidations: checkPassword(formState['password']),
+    };
+
+    // 全てのバリデーションエラーを一次元にして、バリデーションが通ったか判定
+    const allValidations = Object.values(validations).flat();
+    const isValid = (allValidations.length == 0)
+    // バリデーションの状態を更新
+    validationDispatch({ 
+      type: 'update_state',
+      ...validations
+    })
+    return isValid
+  }
+
+  const loginUser = async () => {
+    /**
+     * フォームのサブミットボタンが押された時に、バリデーションをチェックしてAPIと通信する
+     * @return {void}
+    */
+
+    if (!validateForm()) return;
+    try {
+      // バックエンドAPIにフォームをPOSTしてレスポンスを受け取る
+      let {httpStatus, statusText, data } = await postToLoginApi(
+        { 'email': formState['email'], 'password': formState['password'] })
+      if(httpStatus == 201){
+        // 無事ユーザーが作成された場合、ログイン状態を更新してリダイレクト
+        setLoginFlg(() => true)
+        redirectToIndexPage()
+      } else if([400,401,404].includes(httpStatus)) {
+        /**
+         * 400: バックエンドAPIのバリデーションエラー
+         * 401: 認証エラー
+         * 404: Userが存在しない
+         *  */
+        validationDispatch({ 
+          type:'update_state',
+          ...validationStates,
+          emailValidations:    data['email'] || [],
+          passwordValidations: data['password'] || [],
+        })
+      } else{
+        // バックエンド側での予期せぬエラーの場合
+        console.error(`httpStatus: ${httpStatus}, statusText: ${statusText}, data: ${data}`)
+        //未実装
+      }
+    } 
+    catch (error) {
+      // フロントエンド側での予期せぬエラーの場合
+      console.error('Login error:', error)
+      //未実装
+    }
+  };
+
+  return {
+    loginFlg,
+    formState,
+    formDispatch,
+    validationStates,
+    loginUser,
+  };
+}
---
+++ nextjs/src/nextjs_accounts/tests/hooks/useLogin.test.tsx
@@ -0,0 +1,132 @@
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook, act } from '@testing-library/react'
+import { useLogin } from '../../hooks/useLogin'
+
+// Next.jsのuseRouterモジュールのモック
+let routerPushMock = jest.fn()
+jest.mock("next/navigation", () => ({
+    ...jest.requireActual("next/navigation"),
+    useRouter: ()=> {return { push: routerPushMock }},
+  })
+)
+
+//コンテキストのモック
+const setLoginFlgMock = jest.fn().mockReturnValue(true)
+jest.mock('../../hooks/LoginFlgContext',
+  ()=>({...jest.requireActual('../../hooks/LoginFlgContext'),
+    useLoginFlgContext : ()=>false,
+    useSetLoginFlgContext : ()=>setLoginFlgMock
+  })
+)
+
+const postToLoginApiMock = jest.fn().mockResolvedValue({ httpStatus: 201, statusText: "", data: {}})
+//APIと通信するモジュールのモック
+jest.mock('../../hooks/useFetch',
+  ()=>({...jest.requireActual('../../hooks/useFetch'),
+    useFetch: ()=>{
+      return { postToLoginApi : async()=>postToLoginApiMock() }
+    }
+  })
+)
+
+describe('useLogin', () => {
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+  test('バリデーションが通らない時に、処理が中断されている',async()=>{
+    const { result } = renderHook(() => useLogin())
+    // フォームに無効な値を入力してログイン処理
+    await act(() => {
+      result.current.formDispatch({ type: 'edited_email', email: '' });
+      result.current.formDispatch({ type: 'edited_password', password: '' });
+    })
+    await act(() => {
+      result.current.loginUser();
+    })
+    // バリデーションが更新されている
+    expect(result.current.validationStates.emailValidations).toContain('メールアドレスを入力してください')
+    expect(result.current.validationStates.passwordValidations).toContain('パスワードを入力してください')
+    // バリデーションが通らずにAPIと通信する関数が呼び出されていない
+    expect(postToLoginApiMock).not.toHaveBeenCalled()
+    // ログイン状態を変更する関数が呼び出されていない
+    expect(setLoginFlgMock).not.toHaveBeenCalled()
+    // router.pushメソッドが呼び出されていない
+    expect(routerPushMock).not.toHaveBeenCalled()
+  })
+
+  test('ログインに成功した時、APIと通信され、ログイン状態が更新されて、rooter.pushメソッドが実行される',async()=>{
+    const { result } = renderHook(() => useLogin())
+    // フォームに妥当な値を入力してログイン処理
+    await act(() => {
+      result.current.formDispatch({ type: 'edited_email', email: 'testemail@example.com' });
+      result.current.formDispatch({ type: 'edited_password', password: 'password' });
+    })
+    await act(() => {
+      result.current.loginUser();
+    })
+    // APIと通信する関数が呼び出されている
+    expect(postToLoginApiMock).toHaveBeenCalled()
+    // ログイン状態を変更する関数が呼び出されている
+    expect(setLoginFlgMock).toHaveBeenCalled()
+    // router.pushメソッドが呼び出されている
+    expect(routerPushMock).toHaveBeenCalled()
+  })
+
+  test('APIから401エラーが帰ってきた時に、カスタムフックの持つバリデーションの値が更新される',async()=>{
+    // APIから受け取る値を変更
+    postToLoginApiMock.mockResolvedValue({ 
+      httpStatus:401, 
+      statusText:'',
+      data: { password: ['パスワードが間違っています']}
+    })
+    const { result } = renderHook(() => useLogin())
+    // フォームに妥当な値を入力してログイン処理
+    await act(() => {
+      result.current.formDispatch({ type: 'edited_email', email: 'testemail@example.com' });
+      result.current.formDispatch({ type: 'edited_password', password: 'password' });
+    })
+    await act(() => {
+      result.current.loginUser();
+    })
+    // バリデーションが更新されている
+    expect(result.current.validationStates.passwordValidations).toContain('パスワードが間違っています')
+
+    // APIと通信する関数が呼び出されている
+    expect(postToLoginApiMock).toHaveBeenCalled()
+    // ログイン状態を変更する関数が呼び出されていない
+    expect(setLoginFlgMock).not.toHaveBeenCalled()
+    // router.pushメソッドが呼び出されていない
+    expect(routerPushMock).not.toHaveBeenCalled()
+  })
+
+  test('APIから404エラーが返ってきた時に、カスタムフックの持つバリデーションの値が更新される',async()=>{
+    // APIから受け取る値を変更
+    postToLoginApiMock.mockResolvedValue({ 
+      httpStatus:404, 
+      statusText:'',
+      data: { email: ['メールアドレスが間違っています']}
+    })
+    const { result } = renderHook(() => useLogin())
+    // フォームに妥当な値を入力してログイン処理
+    await act(() => {
+      result.current.formDispatch({ type: 'edited_email', email: 'testemail@example.com' });
+      result.current.formDispatch({ type: 'edited_password', password: 'password' });
+    })
+    await act(() => {
+      result.current.loginUser();
+    })
+    // バリデーションが更新されている
+    expect(result.current.validationStates.emailValidations).toContain('メールアドレスが間違っています')
+
+    // APIと通信する関数が呼び出されている
+    expect(postToLoginApiMock).toHaveBeenCalled()
+    // ログイン状態を変更する関数が呼び出されていない
+    expect(setLoginFlgMock).not.toHaveBeenCalled()
+    // router.pushメソッドが呼び出されていない
+    expect(routerPushMock).not.toHaveBeenCalled()
+  })
+});
