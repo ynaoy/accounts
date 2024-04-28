1d3ef541ad7b0b38adea7c37cff8d75b028f062d
ynaoy <naozou22113@yahoo.co.jp>
2024-04-28 13:15:46 +0900

    useFetchフックをclientとserver側それぞれの環境に対応させるため二つのファイルに分割した。それに伴った軽微な修正をした

--- nextjs/src/nextjs_accounts/app/api/login/check/route.ts
+++ nextjs/src/nextjs_accounts/app/api/login/check/route.ts
@@ -2,11 +2,11 @@
 
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { useFetch } from '../../../../hooks/useFetch';
+import { useFetchOnServer } from '../../../../hooks/useFetchOnServer';
 
 export async function GET(req: NextRequest){
   // 外部APIと通信する関数を受け取る
-  const { fetchLoginFlgFromBackendServer } = useFetch()
+  const { fetchLoginFlgFromBackendServer } = useFetchOnServer()
   // 外部APIからログイン状態を受け取る
   const jsonData = await fetchLoginFlgFromBackendServer()
   return NextResponse.json(jsonData)
