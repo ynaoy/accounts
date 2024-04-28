62146874870fc42ca61849cb38727ec23a3b447a
ynaoy <naozou22113@yahoo.co.jp>
2024-04-28 12:58:27 +0900

    clientとserverでのfetchをそれぞれ二つのファイルに分割した。

--- nextjs/src/nextjs_accounts/hooks/useFetch.tsx
+++ nextjs/src/nextjs_accounts/hooks/useFetch.tsx
@@ -1,22 +1,14 @@
 import {  fetchLoginFlgReturnType, fetchParamsType, 
-  SignupParamsType, SignupReturnType,
-  LoginParamsType, LoginReturnType } from './types/useFetch.d'
-import { getCookie as getCookieOnClient } from 'cookies-next';
-import { cookies as getCookieOnServer} from 'next/headers'
+  signupParamsType, signupToFrontendServerReturnType,
+  loginParamsType, loginToFrontendServerReturnType } from './types/useFetch.d'
 
-const fetchResponse = async(url:string, 
-                            params:fetchParamsType, 
-                            data: Record<string, unknown> = {}) =>{
+export const fetchResponse = async( url:string, 
+                                    params:fetchParamsType, 
+                                    data: Record<string, unknown> = {}) =>{
   /**
    * APIと通信する基本的な関数
    * @return { Response }
   */
-
-  // クッキーを取得
-  const auth = getCookie('Authorization'); // 存在しなければundefined
-  if(auth!=undefined){
-    params.headers['Authorization'] = `${process.env.NEXT_PUBLIC_JWT_AUTH_HEADER_TYPES} ${auth}`
-  }
 
   // Json形式でリクエストをする
   params.headers['Content-Type'] = 'application/json';
@@ -30,23 +22,6 @@ const fetchResponse = async(url:string,
   return response
 }
 
-const getCookie =(key:string)=>{
-  /**
-   * クライアントかサーバーそれぞれでの実行環境に合わせてクッキーを取得する
-   * @return { string|undefined }
-  */
-
-  // windowが使えればクライアント、そうでなければサーバー上でのクッキー取得
-  const isClient = (typeof window !== 'undefined') 
-  // クッキーを取得
-  if(isClient) { 
-    return getCookieOnClient(key)// 存在しなければundefined
-  }else{
-    const auth = getCookieOnServer().get(key)  // 存在しなければundefined
-    return auth? auth.value: undefined
-  }
-}
-
 export const useFetch = ()=> {
 
   const fetchLoginFlgFromFrontendServer:fetchLoginFlgReturnType =async()=>{
@@ -54,12 +29,12 @@ export const useFetch = ()=> {
      * フロントエンドサーバーにリクエストを送って、ログイン状態を受け取る 
      * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
     */
-  
+
     // フロントエンドサーバーにリクエストを送る
     try{
       const response = await fetchResponse(
-        'api/login/check',
-        { method:'GET', headers: {}, credentials: 'include',} 
+        `${process.env.NEXT_PUBLIC_MY_ORIGIN}/api/login/check/`,
+        { method:'GET', headers: {}, credentials: 'include',}
       )
       return await response.json()
   
@@ -69,37 +44,7 @@ export const useFetch = ()=> {
     }
   }
 
-  const fetchLoginFlgFromBackendServer:fetchLoginFlgReturnType =async()=>{
-    /**
-     * バックエンドサーバーにリクエストを送って、ログイン状態を受け取る 
-     * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
-    */
-  
-    // バックエンドサーバーにリクエストを送る
-    try{
-      const response = await fetchResponse(
-        `${process.env.NEXT_PUBLIC_API_ORIGIN}/api/is_login`,
-        { method:'GET', headers: {}, credentials: 'include',} 
-      )
-      let ret = await response.json()// { loginFlg: {boolean}}
-      return {
-        httpStatus: response.status,
-        statusText: response.statusText,
-        data:ret
-      }
-  
-    } catch(error) {
-      console.error(error);
-      return { 
-        // 汎用的なエラーレスポンスを返す
-        httpStatus: 500,
-        statusText: 'Internal Server Error',
-        data: { loginFlg: false, message: '予期せぬエラーが発生しました' },
-      };
-    }
-  }
-
-  const signupToFrontendServer:SignupReturnType =async(data: SignupParamsType)=>{
+  const signupToFrontendServer:signupToFrontendServerReturnType =async(data: signupParamsType)=>{
     /**
      * フロントエンドサーバーにPOSTメソッドでリクエストを送る
      * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
@@ -107,7 +52,10 @@ export const useFetch = ()=> {
     
     // フロントエンドサーバーにリクエストを送る
     try{
-      const response = await fetchResponse('api/signup/', { method:'POST', headers: {}, credentials: 'include', }, data )
+      const response = await fetchResponse(
+        `${process.env.NEXT_PUBLIC_MY_ORIGIN}/api/signup/`,
+        { method:'POST', headers: {}, credentials: 'include', },
+        data )
       return await response.json()
 
     } catch(error) {
@@ -120,40 +68,7 @@ export const useFetch = ()=> {
     }
   }
 
-  const signupToBackendServer:SignupReturnType =async(data: SignupParamsType)=>{
-    /**
-     * バックエンドサーバーにPOSTメソッドでリクエストを送る
-     * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
-    */
-    
-    // バックエンドサーバーにリクエストを送る
-    try{
-      const response = await fetchResponse(
-      `${process.env.NEXT_PUBLIC_API_ORIGIN}/api/signup/`,
-      { method:'POST', headers: {}, credentials: 'include', },
-      data)
-
-      //jsonを解凍してリターン 
-      let ret = await response.json()
-      console.log(ret)
-      return {
-        httpStatus: response.status,
-        statusText: response.statusText,
-        data: ret
-      }    
-
-    } catch(error) {
-      console.error('Undefind Error:', error);
-      return { 
-        // 汎用的なエラーレスポンスを返す
-        httpStatus: 500,
-        statusText: 'Internal Server Error',
-        data: { message: '予期せぬエラーが発生しました' },
-      };
-    }
-  }
-
-  const loginToFrontendServer:LoginReturnType =async(data: LoginParamsType)=>{
+  const loginToFrontendServer:loginToFrontendServerReturnType =async(data: loginParamsType)=>{
     /**
      * フロントエンドサーバーにPOSTメソッドでリクエストを送る
      * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
@@ -162,41 +77,10 @@ export const useFetch = ()=> {
     // APIにリクエストを送る
     try{
       const response = await fetchResponse(
-      'api/login/',
-      { method:'POST', headers: {}, credentials: 'include', },
-      data)
+        `${process.env.NEXT_PUBLIC_MY_ORIGIN}/api/login/`,
+        { method:'POST', headers: {}, credentials: 'include', },
+        data)
       return await response.json()
-    } catch(error) {
-      console.error('Undefind Error:', error);
-      return { 
-        // 汎用的なエラーレスポンスを返す
-        httpStatus: 500,
-        statusText: 'Internal Server Error',
-        data: { message: '予期せぬエラーが発生しました' },
-      };
-    }
-  }
-  const loginToBackendServer:LoginReturnType =async(data: LoginParamsType)=>{
-    /**
-     * バックエンドサーバーにPOSTメソッドでリクエストを送る
-     * @return {httpStatus: number, statusText: string, data: {[key:string]: string[]}}
-    */
-    
-    // APIにリクエストを送る
-    try{
-      const response = await fetchResponse(
-      `${process.env.NEXT_PUBLIC_API_ORIGIN}/api/login/`,
-      { method:'POST', headers: {}, credentials: 'include', },
-      data)
-
-      //jsonを解凍してリターン 
-      let ret = await response.json()
-      console.log(ret)
-      return {
-        httpStatus: response.status,
-        statusText: response.statusText,
-        data: ret
-      }    
     } catch(error) {
       console.error('Undefind Error:', error);
       return { 
@@ -210,10 +94,7 @@ export const useFetch = ()=> {
 
   return{
     fetchLoginFlgFromFrontendServer,
-    fetchLoginFlgFromBackendServer,
     signupToFrontendServer,
-    signupToBackendServer,
     loginToFrontendServer,
-    loginToBackendServer
   }
 }
