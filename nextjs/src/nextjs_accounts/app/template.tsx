f0e2c117d217bb508e6d03d0c6ae4ecb585497ff
ynaoy <naozou22113@yahoo.co.jp>
2024-04-28 15:03:59 +0900

    useFetchフックの仕様変更に伴った修正をした。またContextを親コンポーネントに移動してバグを修正した

--- nextjs/src/nextjs_accounts/app/template.tsx
+++ nextjs/src/nextjs_accounts/app/template.tsx
@@ -3,21 +3,20 @@
  * コンポーネントをラップするテンプレート
  * 画面の遷移時に再レンダーされる
  */
-import 'tailwindcss/tailwind.css'
 import { useEffect } from 'react'
 import { useFetch  } from '../hooks/useFetch';
-import { LoginFlgContextProvider, useSetLoginFlgContext } from '../hooks/LoginFlgContext'
+import { useSetLoginFlgContext } from '../hooks/LoginFlgContext'
 
 export default function RootTemplate({children,}: {children: React.ReactNode}) {
   // APIと通信する関数を受け取る
-  const { fetchLoginFlg } = useFetch()
+  const { fetchLoginFlgFromFrontendServer } = useFetch()
   // グローバル変数からloginFlgの更新用関数を取得
   const setLoginFlg = useSetLoginFlgContext()
 
   // APIと通信して、LoginFlgを更新する関数
   const fetchData = async()=>{
-    let {loginFlg} = await fetchLoginFlg()
-    setLoginFlg(loginFlg)
+    let {data} = await fetchLoginFlgFromFrontendServer()
+    setLoginFlg(()=>data['loginFlg'])
   }
   useEffect(()=>{
     fetchData()
@@ -25,9 +24,7 @@ export default function RootTemplate({children,}: {children: React.ReactNode}) {
 
   return (
     <div className="bg-white dark:bg-slate-800 flex flex-col justify-center items-center h-screen w-screen">
-      <LoginFlgContextProvider>
-        {children}
-      </LoginFlgContextProvider>
+      {children}
     </div>
   )
 }
