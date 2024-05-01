dd99e976f44f12b7a27b7365523f7218c135235e
ynaoy <naozou22113@yahoo.co.jp>
2024-05-01 13:27:02 +0900

    UpdateViewのテストを追加した

--- django/src/django_accounts/main_app/tests.py
+++ django/src/django_accounts/main_app/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .test.models_tests import UserModelTests
 from .test.utils_tests import UserUtilsTests
-from .test.views_tests import IsLoginViewsTest, SignupViewTests,LoginViewTests
+from .test.views_tests import IsLoginViewsTest, SignupViewTests, LoginViewTests, UpdateViewTests
 
 class Tests(TestCase):
   UserModelTests()
@@ -9,3 +9,4 @@ class Tests(TestCase):
   IsLoginViewsTest()
   SignupViewTests()
   LoginViewTests()
+  UpdateViewTests()
