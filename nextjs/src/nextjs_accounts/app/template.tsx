f30b8fa5db9febeaebe186b77702c70277b35b92
ynaoy <naozou22113@yahoo.co.jp>
2024-05-14 17:04:41 +0900

    APIからログイン情報を受け取ってから子コンポーネントが表示されるようにした

--- nextjs/src/nextjs_accounts/app/template.tsx
+++ nextjs/src/nextjs_accounts/app/template.tsx
@@ -3,7 +3,7 @@
  * コンポーネントをラップするテンプレート
  * 画面の遷移時に再レンダーされる
  */
-import { useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useFetch  } from '../hooks/useFetch';
 import { useSetLoginFlgContext } from '../hooks/LoginFlgContext'
 
@@ -12,19 +12,39 @@ export default function RootTemplate({children,}: {children: React.ReactNode}) {
   const { fetchLoginFlgFromFrontendServer } = useFetch()
   // グローバル変数からloginFlgの更新用関数を取得
   const setLoginFlg = useSetLoginFlgContext()
+  // データのfetchが完了するまでローディング画面を表示しておく
+  const [isLoading, setIsLoading] = useState(true);
+  // マウント状態を管理する。更新時に再レンダーして欲しくないのでRefを使用
+  const isMounted = useRef(false);
 
-  // APIと通信して、LoginFlgを更新する関数
   const fetchData = async()=>{
-    let {data} = await fetchLoginFlgFromFrontendServer()
-    setLoginFlg(()=>data['loginFlg'])
+    // APIと通信して、LoginFlgを更新する関数
+    setIsLoading(true);
+    try {
+      const { data } = await fetchLoginFlgFromFrontendServer();
+      if (isMounted.current) {
+        // コンポーネントが既にアンマウントされていたらログイン状態を変更しない
+        setLoginFlg(() => data['loginFlg']);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   }
+  
   useEffect(()=>{
+    // コンポーネントのマウント状態を更新
+    isMounted.current = true; 
+    // ログイン状態の更新
     fetchData()
+    return () => {
+      // コンポーネントがアンマウントされた際にログイン情報を更新しないようにする
+      isMounted.current = false;
+    };
   },[])
 
   return (
     <div className="bg-white dark:bg-slate-800 flex flex-col justify-center items-center h-screen w-screen">
-      {children}
+      { isLoading? "Loading...": children }
     </div>
   )
 }
